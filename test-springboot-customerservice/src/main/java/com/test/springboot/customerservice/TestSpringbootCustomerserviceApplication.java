55001795569eddaf2b236cde5f533811d161b695
M1038833 <M1038833@A2ML21162.mindtree.com>
2017-09-19 16:08:29 +0530

    Initial commit

---
+++ test-springboot-customerservice/src/main/java/com/test/springboot/customerservice/TestSpringbootCustomerserviceApplication.java
@@ -0,0 +1,12 @@
+package com.test.springboot.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringbootCustomerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringbootCustomerserviceApplication.class, args);
+	}
+}
